1a8971985c41778df03b9e6c83096683a0014cb6
Vidiic <jordividal1997@gmail.com>
2023-05-30 23:18:25 +0200

    form login integration

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['../auth.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit, OnDestroy {
+  loginForm!: FormGroup;
+  isValidForm!: boolean;
+  private destroy$ = new Subject<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loginForm = this.getForm;
+    this.isValidForm = this.loginForm.valid;
+    this.formStatusChange();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  get getForm(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  formStatusChange(): void {
+    this.loginForm.statusChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((status) => {
+        this.isValidForm = status === 'VALID';
+      });
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm);
+  }
+}
